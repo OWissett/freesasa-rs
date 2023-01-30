e96d4d4d5717d8b59145859bfa6071a5b06a9c14
Oliver Wissett <wissettol@gmail.com>
2023-01-30 18:37:32 +0000

    Set freesasa.a by Env var

--- build.rs
+++ build.rs
@@ -4,8 +4,15 @@ use std::env;
 use std::path::PathBuf;
 
 fn main() {
-    // Tell cargo to look for shared libraries in the directory
-    println!("cargo:rustc-link-search=/usr/local/lib");
+    if let Some(freesasa_location) = option_env!("FREESASA_STATIC_LIB")
+    {
+        println!(
+            "cargo:rustc-link-search=native={}",
+            freesasa_location
+        );
+    } else {
+        println!("cargo:rustc-link-search=native=/usr/local/lib");
+    }
 
     println!("cargo:rustc-link-lib=static=freesasa");
 
