9d1ca411ecb9a211f1f31e5a9ca8b453cb7f5c96
Oliver Wissett <wissettol@gmail.com>
2023-04-17 16:18:23 +0100

    removed structure uids and fixed algo

--- src/uids.rs
+++ src/uids.rs
@@ -3,7 +3,7 @@ use std::fmt::Display;
 
 use freesasa_sys::{
     freesasa_node, freesasa_node_name, freesasa_node_parent,
-    freesasa_node_residue_number, freesasa_node_structure_model,
+    freesasa_node_residue_number,
 };
 
 use crate::{result::node::NodeType, utils::assert_nodetype};
@@ -12,7 +12,7 @@ use crate::{result::node::NodeType, utils::assert_nodetype};
 type ResID = (i32, Option<char>);
 
 // Structure, chain, residue, atom
-type UidPrimitive = (i32, Option<char>, Option<ResID>, Option<String>);
+type UidPrimitive = (char, Option<ResID>, Option<String>);
 
 /// Unique ID for a structure node (e.g. a chain, residue, atom, etc.).
 #[derive(Debug, PartialEq, Eq, Hash, Clone, PartialOrd, Ord)]
@@ -20,13 +20,8 @@ pub struct NodeUid {
     // NOTE: The ordering of the fields is important, because it determines the
     // precedence of the fields in derive `Ord` and `PartialOrd` implementations.
     //
-    /// This is technically the model number, but it's usually just 1.
-    /// The node name of the structure node in `freesasa-sys` is the
-    /// concatenation of the chain IDs, so we don't really need this.
-    structure: i32,
-
     /// Chain ID.
-    chain: Option<char>,
+    chain: char,
 
     /// Residue ID - tuple of residue number and insertion code.
     res_id: Option<ResID>,
@@ -37,13 +32,11 @@ pub struct NodeUid {
 
 impl NodeUid {
     pub fn new(
-        structure: i32,
-        chain: Option<char>,
+        chain: char,
         res_id: Option<ResID>,
         atom_name: Option<String>,
     ) -> Self {
         Self {
-            structure,
             chain,
             res_id,
             atom_name,
@@ -52,7 +45,7 @@ impl NodeUid {
 
     // Create a new `NodeUid` from a `UidPrimitive`.
     fn from_primitive(
-        (structure, chain, res_id, atom_name): UidPrimitive,
+        (chain, res_id, atom_name): UidPrimitive,
     ) -> Self {
         #[cfg(debug_assertions)]
         {
@@ -61,22 +54,10 @@ impl NodeUid {
                     res_id.is_some(),
                     "Atom name provided without residue ID"
                 );
-                assert!(
-                    chain.is_some(),
-                    "Atom name provided without chain ID"
-                );
-            }
-
-            if res_id.is_some() {
-                assert!(
-                    chain.is_some(),
-                    "Residue ID provided without chain ID"
-                );
             }
         }
 
         Self {
-            structure,
             chain,
             res_id,
             atom_name,
@@ -87,9 +68,10 @@ impl NodeUid {
         let node_type = NodeType::nodetype_of_ptr(node);
 
         match node_type {
-            NodeType::Structure => Some(Self::from_primitive(
-                Self::from_structure_ptr(node),
-            )),
+            NodeType::None => None,
+            NodeType::Root => None,
+            NodeType::Result => None,
+            NodeType::Structure => None,
             NodeType::Chain => {
                 Some(Self::from_primitive(Self::from_chain_ptr(node)))
             }
@@ -99,18 +81,11 @@ impl NodeUid {
             NodeType::Atom => {
                 Some(Self::from_primitive(Self::from_atom_ptr(node)))
             }
-            NodeType::None => None,
-            NodeType::Result => None,
-            NodeType::Root => None,
         }
     }
 
-    pub fn structure(&self) -> i32 {
-        self.structure
-    }
-
-    pub fn chain(&self) -> Option<&char> {
-        self.chain.as_ref()
+    pub fn chain(&self) -> &char {
+        &self.chain
     }
 
     pub fn res_id(&self) -> Option<&ResID> {
@@ -121,38 +96,29 @@ impl NodeUid {
         self.atom_name.as_deref()
     }
 
-    fn from_structure_ptr(node: *mut freesasa_node) -> UidPrimitive {
-        #[cfg(debug_assertions)]
-        assert_nodetype(&node, NodeType::Structure);
-
-        let structure = unsafe { freesasa_node_structure_model(node) };
-
-        (structure, None, None, None)
-    }
-
     fn from_chain_ptr(node: *mut freesasa_node) -> UidPrimitive {
         #[cfg(debug_assertions)]
         assert_nodetype(&node, NodeType::Chain);
 
-        let structure = unsafe {
-            freesasa_node_structure_model(freesasa_node_parent(node))
-        };
         let chain = unsafe { freesasa_node_name(node) };
 
         // convert from c-style string to String
         let chain = unsafe {
-            ffi::CStr::from_ptr(chain).to_str().unwrap().chars().next()
+            match ffi::CStr::from_ptr(chain)
+                .to_str()
+                .unwrap()
+                .chars()
+                .next()
+            {
+                Some(c) => c,
+                None => {
+                    warn!("Chain name contained invalid UTF-8 bytes, using * as chain ID");
+                    '*'
+                }
+            }
         };
 
-        #[cfg(debug_assertions)]
-        {
-            assert!(
-                chain.is_some(),
-                "Chain ID is None, but node type is Chain"
-            );
-        }
-
-        (structure, chain, None, None)
+        (chain, None, None)
     }
 
     fn from_residue_ptr(node: *mut freesasa_node) -> UidPrimitive {
@@ -183,7 +149,7 @@ impl NodeUid {
                 (resnum, Some(inscode))
             };
 
-        uid.2 = Some((resnum.parse().unwrap(), inscode));
+        uid.1 = Some((resnum.parse().unwrap(), inscode));
 
         uid
     }
@@ -203,7 +169,7 @@ impl NodeUid {
             ffi::CStr::from_ptr(atom_name).to_str().unwrap().to_string()
         };
 
-        uid.3 = Some(atom_name);
+        uid.2 = Some(atom_name);
 
         uid
     }
@@ -211,16 +177,9 @@ impl NodeUid {
 
 impl Display for NodeUid {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let mut uid = format!("{}:", self.structure);
+        let mut uid = String::new();
 
-        // Add the chain ID if it exists...
-        if let Some(chain) = self.chain {
-            uid.push(chain);
-        }
-        // ...else return.
-        else {
-            return write!(f, "{}", uid);
-        };
+        uid.push(self.chain);
 
         // Add the residue ID if it exists...
         if let Some((resnum, inscode)) = self.res_id {
